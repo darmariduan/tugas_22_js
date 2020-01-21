db255aa93493b46f59ec5f99e44065c1fc3670db
Darma Riduan <dharmarhiduwhan@gmail.com>
2020-01-21 08:45:01 +0700

    Tugas 22 Js

---
+++ tugas22.js
@@ -0,0 +1,9 @@
+function tugas22() {
+  var kata = "Saya ingin belajar bersama";
+  var kata2= kata.split(" ");
+  kata2.forEach(function(item, index) {
+console.log("Item:", item , "Index ke", index);
+})
+
+}
+tugas22();
